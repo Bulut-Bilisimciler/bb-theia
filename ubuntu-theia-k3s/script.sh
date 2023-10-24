690663afe2a28b7160d4f6643a7d0e34b1eadeeb
enescedev <selimenesc@gmail.com>
2023-10-24 14:04:23 +0300

    pardus-theia docker entrypoint added.

---
+++ ubuntu-theia-k3s/script.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# ENV values PRELOAD_FILE_URL, PRELOAD_FILE_DIR
+# We need to try to wget the file from the URL to the demanded directory.
+# If env's not set skip. If file already overwrite
+# If file could not fetched write "Could not load file contact administrator. https://bulutbilisimciler.com/destek"
+
+if [ -n "$PRELOAD_FILE_URL" ] && [ -n "$PRELOAD_FILE_DIR" ]
+then
+    echo "Downloading file from $PRELOAD_FILE_URL to $PRELOAD_FILE_DIR"
+    if wget -O $PRELOAD_FILE_DIR $PRELOAD_FILE_URL; then
+        echo "Shell OK"
+    else
+        echo "Could not load file. Please contact administrator at https://bulutbilisimciler.com/destek"
+    fi
+fi
+
+node /root/ide/src-gen/backend/main.js --hostname=0.0.0.0 --port=3030 --plugins=local-dir:/root/ide/plugins
