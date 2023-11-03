932d0bb1d99f0adb973aaa4949851051e9378e09
enescedev <selimenesc@gmail.com>
2023-11-04 02:00:30 +0300

    docker-compase yaml updated.

--- ubuntu-theia-docker-compose/init.sql
+++ ubuntu-theia-docker-compose/init.sql
@@ -1,3 +1,6 @@
+CREATE USER bbuser WITH PASSWORD 'bbuserpassword';
+CREATE DATABASE bulutdb;
+GRANT ALL PRIVILEGES ON DATABASE bulutdb TO bbuser;
 
 CREATE TABLE customers (
 CustomerID SERIAL PRIMARY KEY, 
